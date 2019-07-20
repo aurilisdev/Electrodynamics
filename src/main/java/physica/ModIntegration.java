37ecd0da27d262c86aa6d52ec5efec03886a4dee
Sebastian Siljuholtet Johansen <aurilisdev@gmail.com>
2019-07-20 12:57:55 +0200

    Why have all of that api when  you can do it reflectively

--- src/main/java/physica/ModIntegration.java
+++ src/main/java/physica/ModIntegration.java
@@ -1,7 +1,12 @@
 package physica;
 
+import java.lang.reflect.Method;
+
+import org.apache.logging.log4j.Level;
+
 import cpw.mods.fml.common.Loader;
-import mcp.mobius.waila.api.impl.ModuleRegistrar;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import physica.api.core.PhysicaAPI;
 import physica.api.core.electricity.IElectricityProvider;
 import physica.api.core.electricity.IElectricityReceiver;
 import physica.api.core.load.IContent;
@@ -14,13 +19,32 @@ public class ModIntegration implements IContent {
 	{
 		if (phase == LoadPhase.OnStartup)
 		{
+
 			if (Loader.isModLoaded("Waila"))
 			{
-				ModuleRegistrar.instance().addConfigRemote("Physica", "physica.electricityhandler");
-				ModuleRegistrar.instance().registerBodyProvider(new HUDHandlerIElectricityHandler(), IElectricityProvider.class);
-				ModuleRegistrar.instance().registerNBTProvider(new HUDHandlerIElectricityHandler(), IElectricityProvider.class);
-				ModuleRegistrar.instance().registerBodyProvider(new HUDHandlerIElectricityHandler(), IElectricityReceiver.class);
-				ModuleRegistrar.instance().registerNBTProvider(new HUDHandlerIElectricityHandler(), IElectricityReceiver.class);
+				PhysicaAPI.logger.log(Level.INFO, "Attempting to integrate Physica with WAILA.");
+				try
+				{
+
+					Class<?> moduleRegistrar = Class.forName("mcp.mobius.waila.api.impl.ModuleRegistrar");
+					Object instance = moduleRegistrar.getMethod("instance").invoke(null);
+					Method addConfigRemote = moduleRegistrar.getMethod("addConfigRemote", String.class, String.class);
+					Method registerBodyProvider;
+
+					registerBodyProvider = moduleRegistrar.getMethod("registerBodyProvider", IWailaDataProvider.class, Class.class);
+
+					Method registerNBTProvider = moduleRegistrar.getMethod("registerNBTProvider", IWailaDataProvider.class, Class.class);
+					addConfigRemote.invoke(instance, "Physica", "physica.electricityhandler");
+					registerBodyProvider.invoke(instance, new HUDHandlerIElectricityHandler(), IElectricityProvider.class);
+					registerNBTProvider.invoke(instance, new HUDHandlerIElectricityHandler(), IElectricityProvider.class);
+					registerBodyProvider.invoke(instance, new HUDHandlerIElectricityHandler(), IElectricityReceiver.class);
+					registerNBTProvider.invoke(instance, new HUDHandlerIElectricityHandler(), IElectricityReceiver.class);
+					PhysicaAPI.logger.log(Level.INFO, "Integrated Physica with WAILA.");
+				} catch (Exception e)
+				{
+					PhysicaAPI.logger.log(Level.INFO, "Failed to integrate Physica with WAILA.");
+					e.printStackTrace();
+				}
 			}
 		}
 	}
