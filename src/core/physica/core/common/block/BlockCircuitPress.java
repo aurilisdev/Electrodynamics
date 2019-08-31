a815218d9a2aff51b31d8e0a94a3498f666c75b6
aurilisdev <aurilisdev@gmail.com>
2019-08-31 14:21:35 +0200

    Add recipe

--- src/core/physica/core/common/block/BlockCircuitPress.java
+++ src/core/physica/core/common/block/BlockCircuitPress.java
@@ -13,6 +13,6 @@ public class BlockCircuitPress extends BlockMachine {
 	@Override
 	public void registerRecipes()
 	{
-		addRecipe(this, "ILI", "BFB", "WRW", 'F', Blocks.furnace, 'I', Items.iron_ingot, 'L', Blocks.lever, 'B', Blocks.stone_button, 'R', Blocks.redstone_torch);
+		addRecipe(this, "ILI", "BFB", "WRW", 'F', Blocks.furnace, 'I', Items.iron_ingot, 'L', Blocks.lever, 'B', Blocks.stone_button, 'R', Blocks.redstone_torch, 'W', "plateSteel");
 	}
 }
