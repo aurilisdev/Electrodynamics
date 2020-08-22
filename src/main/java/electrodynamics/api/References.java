8ca1f1fa4f9a612dcdc5b10dbb0b6a4e85b26fbf
aurilisdev <aurilisdev@gmail.com>
2020-08-22 15:10:32 +0200

    Update version

--- src/main/java/electrodynamics/api/References.java
+++ src/main/java/electrodynamics/api/References.java
@@ -5,6 +5,6 @@ import electrodynamics.common.tab.ItemGroupElectrodynamics;
 public class References {
 	public static final String ID = "electrodynamics";
 	public static final String NAME = "Electrodynamics";
-	public static final String VERSION = "0.1.4-1.16.1";
+	public static final String VERSION = "0.1.5-1.16.1";
 	public static final ItemGroupElectrodynamics CORETAB = new ItemGroupElectrodynamics("itemgroupelectrodynamics");
 }
