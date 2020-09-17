0081b1ec9df3a3a39e14c84137752971ad73ffcb
aurilisdev <aurilisdev@gmail.com>
2020-09-17 21:09:02 +0200

    Update transformer in-game bounding box

--- src/main/java/electrodynamics/common/block/BlockMachine.java
+++ src/main/java/electrodynamics/common/block/BlockMachine.java
@@ -14,15 +14,24 @@ import net.minecraft.loot.LootContext.Builder;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
 public class BlockMachine extends BlockGenericMachine {
+	private static final VoxelShape transformershape = VoxelShapes.create(0, 0, 0, 1, 15.0 / 16.0, 1);
 	public final SubtypeMachine machine;
 
 	public BlockMachine(SubtypeMachine machine) {
-		super();
 		this.machine = machine;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return machine == SubtypeMachine.downgradetransformer || machine == SubtypeMachine.downgradetransformer ? transformershape : super.getShape(state, worldIn, pos, context);
 	}
 
 	@Override
