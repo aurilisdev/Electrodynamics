9a3c3570f273c402a35c67eea724948ac6d93947
aurilisdev <aurilisdev@gmail.com>
2021-01-01 18:36:58 +0100

    Cleanup

--- src/main/java/electrodynamics/common/damage/DamageSources.java
+++ src/main/java/electrodynamics/common/damage/DamageSources.java
@@ -3,8 +3,6 @@ package electrodynamics.common.damage;
 import net.minecraft.util.DamageSource;
 
 public class DamageSources {
-	public static final DamageSource ELECTRICITY = new DamageSource("electricity").setDamageBypassesArmor()
-			.setMagicDamage();
-	public static final DamageSource RADIATION = new DamageSource("radiation").setDamageBypassesArmor()
-			.setMagicDamage();
+	public static final DamageSource ELECTRICITY = new DamageSource("electricity").setDamageBypassesArmor().setMagicDamage();
+	public static final DamageSource RADIATION = new DamageSource("radiation").setDamageBypassesArmor().setMagicDamage();
 }
