d28adaf88b73cc2e1e95e7ea6acfa80b2b366f85
aurilisdev <aurilisdev@gmail.com>
2021-04-11 20:30:06 +0200

    Do proper resistance again

--- src/main/java/electrodynamics/common/block/subtype/SubtypeWire.java
+++ src/main/java/electrodynamics/common/block/subtype/SubtypeWire.java
@@ -3,18 +3,16 @@ package electrodynamics.common.block.subtype;
 import electrodynamics.api.ISubtype;
 
 // Calculated using https://www.omnicalculator.com/physics/wire-resistance
-// Length defined as if all sides are connected. This number was found to be 2.875 m
-// Cross-area defined as 0.015625 m^2
-// To make resistance relevant we use the calculated values * 1000*5
+// Also manipulated uniformally
 public enum SubtypeWire implements ISubtype {
-    tin(0.02024, 60, false, false), iron(0.017866, 100, false, false), copper(0.0031464, 360, false, false), silver(0.0029256, 600, false, false),
-    gold(0.00449, 1000, false, false), superconductive(0.0, Long.MAX_VALUE, false, false), insulatedtin(SubtypeWire.tin.resistance, 60, true, false),
-    insulatediron(SubtypeWire.iron.resistance, 100, true, false), insulatedcopper(SubtypeWire.copper.resistance, 360, true, false),
-    insulatedsilver(SubtypeWire.silver.resistance, 600, true, false), insulatedgold(SubtypeWire.gold.resistance, 1000, true, false),
-    insulatedsuperconductive(0.0, Long.MAX_VALUE, true, false), logisticstin(SubtypeWire.tin.resistance, 60, true, true),
-    logisticsiron(SubtypeWire.iron.resistance, 100, true, true), logisticscopper(SubtypeWire.copper.resistance, 360, true, true),
-    logisticssilver(SubtypeWire.silver.resistance, 600, true, true), logisticsgold(SubtypeWire.gold.resistance, 1000, true, true),
-    logisticssuperconductive(0.0, Long.MAX_VALUE, true, true);
+    tin(0.02024 / 5.0, 60, false, false), iron(0.017866 / 5.0, 100, false, false), copper(0.0031464 / 5.0, 360, false, false),
+    silver(0.0029256 / 5.0, 600, false, false), gold(0.00449 / 5.0, 1000, false, false), superconductive(0.0, Long.MAX_VALUE, false, false),
+    insulatedtin(SubtypeWire.tin.resistance, 60, true, false), insulatediron(SubtypeWire.iron.resistance, 100, true, false),
+    insulatedcopper(SubtypeWire.copper.resistance, 360, true, false), insulatedsilver(SubtypeWire.silver.resistance, 600, true, false),
+    insulatedgold(SubtypeWire.gold.resistance, 1000, true, false), insulatedsuperconductive(0.0, Long.MAX_VALUE, true, false),
+    logisticstin(SubtypeWire.tin.resistance, 60, true, true), logisticsiron(SubtypeWire.iron.resistance, 100, true, true),
+    logisticscopper(SubtypeWire.copper.resistance, 360, true, true), logisticssilver(SubtypeWire.silver.resistance, 600, true, true),
+    logisticsgold(SubtypeWire.gold.resistance, 1000, true, true), logisticssuperconductive(0.0, Long.MAX_VALUE, true, true);
 
     public final double resistance;
     public final long capacity;
