3a67cff5f0c652f69344e283cdc747345e82e6f8
aurilisdev <aurilisdev@gmail.com>
2021-07-07 15:37:35 +0200

    Lower ore gen

--- src/main/java/electrodynamics/common/block/subtype/SubtypeOre.java
+++ src/main/java/electrodynamics/common/block/subtype/SubtypeOre.java
@@ -4,14 +4,14 @@ import electrodynamics.api.ISubtype;
 import net.minecraftforge.common.ToolType;
 
 public enum SubtypeOre implements ISubtype {
-    copper(ToolType.PICKAXE, 1, 15, 9, 0, 64, 3f, 5f), tin(ToolType.PICKAXE, 1, 8, 9, 0, 64, 3f, 5f),
-    silver(ToolType.PICKAXE, 2, 4, 9, 0, 48, 4f, 5.5f), lead(ToolType.PICKAXE, 2, 6, 7, 0, 64, 4f, 8f),
-    uraninite(ToolType.PICKAXE, 3, 4, 8, 0, 32, 10f, 6.5f), thorianite(ToolType.PICKAXE, 3, 4, 8, 0, 32, 10f, 6.5f),
-    monazite(ToolType.PICKAXE, 2, 9, 8, 0, 32, 6f, 4.5f), vanadinite(ToolType.PICKAXE, 2, 9, 8, 0, 32, 6f, 4.5f),
-    sulfur(ToolType.PICKAXE, 1, 10, 13, 0, 28, 6f, 4.5f), niter(ToolType.PICKAXE, 1, 10, 13, 0, 28, 6f, 4.5f),
-    aluminum(ToolType.PICKAXE, 2, 9, 5, 0, 64, 4f, 4.4f), chromite(ToolType.PICKAXE, 3, 4, 8, 0, 32, 10f, 6.5f),
-    rutile(ToolType.PICKAXE, 3, 4, 8, 0, 32, 10f, 6.5f), halite(ToolType.PICKAXE, 1, 15, 9, 32, 64, 3f, 5f),
-    lepidolite(ToolType.PICKAXE, 2, 3, 9, 0, 64, 4f, 8f);
+    copper(ToolType.PICKAXE, 1, 9, 9, 0, 64, 3f, 5f), tin(ToolType.PICKAXE, 1, 5, 9, 0, 64, 3f, 5f),
+    silver(ToolType.PICKAXE, 2, 3, 9, 0, 48, 4f, 5.5f), lead(ToolType.PICKAXE, 2, 3, 7, 0, 64, 4f, 8f),
+    uraninite(ToolType.PICKAXE, 3, 3, 8, 0, 32, 10f, 6.5f), thorianite(ToolType.PICKAXE, 3, 3, 8, 0, 32, 10f, 6.5f),
+    monazite(ToolType.PICKAXE, 2, 5, 8, 0, 32, 6f, 4.5f), vanadinite(ToolType.PICKAXE, 2, 5, 8, 0, 32, 6f, 4.5f),
+    sulfur(ToolType.PICKAXE, 1, 7, 13, 0, 28, 6f, 4.5f), niter(ToolType.PICKAXE, 1, 6, 13, 0, 28, 6f, 4.5f),
+    aluminum(ToolType.PICKAXE, 2, 5, 5, 0, 64, 4f, 4.4f), chromite(ToolType.PICKAXE, 3, 3, 8, 0, 32, 10f, 6.5f),
+    rutile(ToolType.PICKAXE, 3, 3, 8, 0, 32, 10f, 6.5f), halite(ToolType.PICKAXE, 1, 9, 9, 32, 64, 3f, 5f),
+    lepidolite(ToolType.PICKAXE, 2, 2, 9, 0, 64, 4f, 8f);
 
     public final ToolType harvestTool;
     public final int harvestLevel;
