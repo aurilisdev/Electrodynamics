4d2fbaf6665deecdf94e67ae66954cd7782fa584
skip999 <skiprocks999@gmail.com>
2021-12-08 20:42:39 -0600

    oops

--- src/main/java/electrodynamics/common/block/BlockResource.java
+++ src/main/java/electrodynamics/common/block/BlockResource.java
@@ -1,12 +1,23 @@
 package electrodynamics.common.block;
 
+import java.util.Arrays;
+import java.util.List;
+
 import electrodynamics.common.block.subtype.SubtypeResourceBlock;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootContext.Builder;
 
 public class BlockResource extends Block {
 
     public BlockResource(SubtypeResourceBlock subtype) {
 	super(Block.Properties.of(subtype.getMaterial()).strength(subtype.getHardness(), subtype.getResistance()).sound(subtype.getSoundType()));
+    }
+    
+    @Override
+    public List<ItemStack> getDrops(BlockState state, Builder builder) {
+    	return Arrays.asList(new ItemStack(this));
     }
 
 }
