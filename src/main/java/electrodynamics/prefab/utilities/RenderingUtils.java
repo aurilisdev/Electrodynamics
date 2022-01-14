929bf9f7667b3bfcbc6d98deb7ab2b8cf35aee5e
aurilisdev <aurilisdev@gmail.com>
2022-01-14 02:50:38 +0100

    FINALLY REINTRODUCE STARS

--- src/main/java/electrodynamics/prefab/utilities/RenderingUtils.java
+++ src/main/java/electrodynamics/prefab/utilities/RenderingUtils.java
@@ -1,12 +1,18 @@
 package electrodynamics.prefab.utilities;
 
+import java.util.Random;
+
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Matrix4f;
 import com.mojang.math.Quaternion;
+import com.mojang.math.Vector3f;
 
 import electrodynamics.prefab.block.GenericEntityBlock;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.MultiBufferSource.BufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.ItemTransforms.TransformType;
 import net.minecraft.client.resources.model.BakedModel;
@@ -22,6 +28,52 @@ public class RenderingUtils {
 
 	public static void renderStar(PoseStack stack, MultiBufferSource bufferIn, float time, int starFrags, float r, float g, float b, float a,
 			boolean star) {
+		stack.pushPose();
+		try {
+			float f5 = time / 200.0F;
+			Random random = new Random(432L);
+			VertexConsumer vertexconsumer2 = bufferIn.getBuffer(RenderType.lightning());
+			stack.pushPose();
+			stack.translate(0.0D, -1.0D, 0.0D);
+
+			for (int i = 0; i < starFrags; ++i) {
+				stack.mulPose(Vector3f.XP.rotationDegrees(random.nextFloat() * 360.0F));
+				stack.mulPose(Vector3f.YP.rotationDegrees(random.nextFloat() * 360.0F));
+				stack.mulPose(Vector3f.ZP.rotationDegrees(random.nextFloat() * 360.0F));
+				stack.mulPose(Vector3f.XP.rotationDegrees(random.nextFloat() * 360.0F));
+				stack.mulPose(Vector3f.YP.rotationDegrees(random.nextFloat() * 360.0F));
+				stack.mulPose(Vector3f.ZP.rotationDegrees(random.nextFloat() * 360.0F + f5 * 90.0F));
+				float f3 = random.nextFloat() * 20.0F + 1.0F;
+				float f4 = random.nextFloat() * 2.0F + 1.0F + (star ? 0 : 100);
+				Matrix4f matrix4f = stack.last().pose();
+				vertexconsumer2.vertex(matrix4f, 0.0F, 0.0F, 0.0F).color((int) (255 * r), (int) (255 * g), (int) (255 * b), (int) (255 * a))
+						.endVertex();
+				vertexconsumer2.vertex(matrix4f, -0.866f * f4, f3, -0.5F * f4)
+						.color((int) (255 * r), (int) (255 * g), (int) (255 * b), (int) (255 * a)).endVertex();
+				vertexconsumer2.vertex(matrix4f, -0.866f * f4, f3, -0.5F * f4)
+						.color((int) (255 * r), (int) (255 * g), (int) (255 * b), (int) (255 * a)).endVertex();
+				vertexconsumer2.vertex(matrix4f, 0.0F, 0.0F, 0.0F).color((int) (255 * r), (int) (255 * g), (int) (255 * b), (int) (255 * a))
+						.endVertex();
+				vertexconsumer2.vertex(matrix4f, -0.866f * f4, f3, -0.5F * f4)
+						.color((int) (255 * r), (int) (255 * g), (int) (255 * b), (int) (255 * a)).endVertex();
+				vertexconsumer2.vertex(matrix4f, 0.0F, f3, 1.0F * f4).color((int) (255 * r), (int) (255 * g), (int) (255 * b), (int) (255 * a))
+						.endVertex();
+				vertexconsumer2.vertex(matrix4f, 0.0F, 0.0F, 0.0F).color((int) (255 * r), (int) (255 * g), (int) (255 * b), (int) (255 * a))
+						.endVertex();
+				vertexconsumer2.vertex(matrix4f, -0.866f * f4, f3, -0.5F * f4)
+						.color((int) (255 * r), (int) (255 * g), (int) (255 * b), (int) (255 * a)).endVertex();
+			}
+
+			stack.popPose();
+			if (bufferIn instanceof BufferSource source) {
+				source.endBatch(RenderType.lightning());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		stack.popPose();
+
 //	stack.pushPose();
 		// TODO: Doesnt render anymore, but doesnt crash either
 //	Tesselator tessellator = Tesselator.getInstance();
