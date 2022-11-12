02120be53947188e0ce177d16c92be9872c723a9
aurilisdev <aurilisdev@gmail.com>
2022-11-12 12:47:51 +0100

    Improve multiblock subnode functions

--- src/main/java/electrodynamics/common/block/BlockMultiSubnode.java
+++ src/main/java/electrodynamics/common/block/BlockMultiSubnode.java
@@ -7,6 +7,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
@@ -16,6 +17,7 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -53,6 +55,16 @@ public class BlockMultiSubnode extends BaseEntityBlock implements IMultiblockSub
 	@Override
 	public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
 		return true;
+	}
+
+	@Override
+	public ItemStack getCloneItemStack(BlockState state, HitResult target, BlockGetter level, BlockPos pos, Player player) {
+		BlockEntity tile = level.getBlockEntity(pos);
+		if (tile instanceof TileMultiSubnode subnode && subnode.nodePos != null) {
+			BlockState core = level.getBlockState(subnode.nodePos.toBlockPos());
+			return new ItemStack(core.getBlock());
+		}
+		return super.getCloneItemStack(state, target, level, pos, player);
 	}
 
 	@Override
