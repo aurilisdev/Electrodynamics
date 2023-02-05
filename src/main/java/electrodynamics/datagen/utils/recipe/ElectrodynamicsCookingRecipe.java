d8d3079d1eb4613a8eab69fd6e8ebe1ab459c1c0
skip999 <skiprocks999@gmail.com>
2023-02-05 15:28:17 -0600

    dealing with mojank

--- src/main/java/electrodynamics/datagen/utils/recipe/ElectrodynamicsCookingRecipe.java
+++ src/main/java/electrodynamics/datagen/utils/recipe/ElectrodynamicsCookingRecipe.java
@@ -18,8 +18,7 @@ public class ElectrodynamicsCookingRecipe extends SimpleCookingRecipeBuilder.Res
 
 	public static float expereince;
 
-	private ElectrodynamicsCookingRecipe(ResourceLocation id, Ingredient input, Item result, float experience,
-			int cookingTime, RecipeSerializer<? extends AbstractCookingRecipe> serializer) {
+	private ElectrodynamicsCookingRecipe(ResourceLocation id, Ingredient input, Item result, float experience, int cookingTime, RecipeSerializer<? extends AbstractCookingRecipe> serializer) {
 		super(id, "", input, result, experience, cookingTime, null, null, serializer);
 	}
 
@@ -27,15 +26,15 @@ public class ElectrodynamicsCookingRecipe extends SimpleCookingRecipeBuilder.Res
 	public JsonObject serializeAdvancement() {
 		return null;
 	}
-	
+
 	public static SmeltingBuilder smeltingRecipe(Item result, float experience, int smeltTime) {
 		return new SmeltingBuilder(result, experience, smeltTime);
 	}
-	
+
 	public static SmokingBuilder smokingRecipe(Item result, float experience, int smeltTime) {
 		return new SmokingBuilder(result, experience, smeltTime);
 	}
-	
+
 	public static BlastingBuilder blastingRecipe(Item result, float experience, int smeltTime) {
 		return new BlastingBuilder(result, experience, smeltTime);
 	}
@@ -47,43 +46,43 @@ public class ElectrodynamicsCookingRecipe extends SimpleCookingRecipeBuilder.Res
 		private final int smeltTime;
 		private final RecipeSerializer<? extends AbstractCookingRecipe> serializer;
 		private Ingredient input;
-		
+
 		private Builder(Item result, float experience, int smeltTime, RecipeSerializer<? extends AbstractCookingRecipe> serializer) {
 			this.result = result;
 			this.experience = experience;
 			this.smeltTime = smeltTime;
 			this.serializer = serializer;
 		}
-		
+
 		public Builder input(Item item) {
-			return input(new ItemStack(item));	
+			return input(new ItemStack(item));
 		}
-		
+
 		public Builder input(ItemStack item) {
 			return input(Ingredient.of(item));
 		}
-		
+
 		public Builder input(TagKey<Item> tag) {
 			return input(Ingredient.of(tag));
 		}
-		
+
 		public Builder input(Ingredient item) {
 			input = item;
 			return this;
 		}
-		
-		public void complete(String parent, String name, Consumer<FinishedRecipe> consumer){
+
+		public void complete(String parent, String name, Consumer<FinishedRecipe> consumer) {
 			consumer.accept(new ElectrodynamicsCookingRecipe(new ResourceLocation(parent, name), input, result, experience, smeltTime, serializer));
 		}
-		
+
 	}
 
 	public static class SmeltingBuilder extends Builder {
-		
+
 		private SmeltingBuilder(Item result, float experience, int smeltTime) {
 			super(result, expereince, smeltTime, RecipeSerializer.SMELTING_RECIPE);
 		}
-		
+
 	}
 
 	public static class SmokingBuilder extends Builder {
@@ -91,15 +90,15 @@ public class ElectrodynamicsCookingRecipe extends SimpleCookingRecipeBuilder.Res
 		private SmokingBuilder(Item result, float experience, int smeltTime) {
 			super(result, expereince, smeltTime, RecipeSerializer.SMOKING_RECIPE);
 		}
-		
+
 	}
 
 	public static class BlastingBuilder extends Builder {
-		
+
 		private BlastingBuilder(Item result, float experience, int smeltTime) {
 			super(result, expereince, smeltTime, RecipeSerializer.BLASTING_RECIPE);
 		}
-		
+
 	}
 
 }
---
+++ src/main/java/electrodynamics/datagen/utils/recipe/ElectrodynamicsSingleItemRecipeBuilder.java
@@ -0,0 +1,50 @@
+package electrodynamics.datagen.utils.recipe;
+
+import java.util.function.Consumer;
+
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.SingleItemRecipeBuilder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+
+/**
+ * Imagine mojank providing a working data generator that didn't have the recipe book hard-coded into it
+ * 
+ * @author skip999
+ *
+ */
+public class ElectrodynamicsSingleItemRecipeBuilder extends SingleItemRecipeBuilder.Result {
+
+	public ElectrodynamicsSingleItemRecipeBuilder(ResourceLocation id, RecipeSerializer<?> recipe, Ingredient input, Item result, int count) {
+		super(id, recipe, "", input, result, count, null, null);
+	}
+	
+	public static Builder stonecuttingRecipe(Ingredient input, Item output, int count) {
+		return new Builder(input, output, count, RecipeSerializer.STONECUTTER);
+	}
+	
+	public static class Builder {
+		
+		private final Item result;
+		private final int count;
+		private final RecipeSerializer<?> serializer;
+		private final Ingredient input;
+
+		public Builder(Ingredient input, Item result, int count, RecipeSerializer<?> serializer) {
+			this.result = result;
+			this.count = count;
+			this.serializer = serializer;
+			this.input = input;
+		}
+
+		public void complete(String parent, String name, Consumer<FinishedRecipe> consumer) {
+			consumer.accept(new ElectrodynamicsSingleItemRecipeBuilder(new ResourceLocation(parent, name), serializer, input, result, count));
+		}
+		
+	}
+
+	
+
+}
