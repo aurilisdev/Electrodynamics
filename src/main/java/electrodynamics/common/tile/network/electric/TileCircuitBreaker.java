683d9e5db6453386b30e7eb84140398d07f6b6bc
skip999 <skiprocks999@gmail.com>
2023-09-18 19:54:25 -0500

    voxel shapes

--- src/main/java/electrodynamics/common/tile/network/electric/TileCircuitBreaker.java
+++ src/main/java/electrodynamics/common/tile/network/electric/TileCircuitBreaker.java
@@ -4,6 +4,8 @@ import org.jetbrains.annotations.NotNull;
 
 import electrodynamics.api.capability.ElectrodynamicsCapabilities;
 import electrodynamics.api.capability.types.electrodynamic.ICapabilityElectrodynamic.LoadProfile;
+import electrodynamics.common.block.VoxelShapes;
+import electrodynamics.common.block.subtype.SubtypeMachine;
 import electrodynamics.common.settings.Constants;
 import electrodynamics.prefab.tile.GenericTile;
 import electrodynamics.prefab.tile.components.ComponentType;
@@ -18,8 +20,11 @@ import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class TileCircuitBreaker extends GenericTile {
 
@@ -200,6 +205,19 @@ public class TileCircuitBreaker extends GenericTile {
 			}
 			setChanged();
 		}
+	}
+	
+	static {
+		
+		VoxelShape shape = Block.box(0, 0, 0, 16, 2, 16);
+		shape = Shapes.or(shape, Block.box(0, 2, 1, 16, 5, 15));
+		shape = Shapes.or(shape, Block.box(0, 2, 1, 16, 5, 15));
+		shape = Shapes.or(shape, Block.box(1, 5, 2, 15, 15, 14));
+		shape = Shapes.or(shape, Block.box(0, 5, 4, 1, 12, 12));
+		shape = Shapes.or(shape, Block.box(15, 5, 4, 16, 12, 12));
+		
+		VoxelShapes.registerShape(SubtypeMachine.circuitbreaker, shape, Direction.WEST);
+		
 	}
 
 }
--- src/main/java/electrodynamics/common/tile/network/electric/TilePotentiometer.java
+++ src/main/java/electrodynamics/common/tile/network/electric/TilePotentiometer.java
@@ -1,18 +1,25 @@
 package electrodynamics.common.tile.network.electric;
 
+import java.util.stream.Stream;
+
 import electrodynamics.api.capability.types.electrodynamic.ICapabilityElectrodynamic.LoadProfile;
+import electrodynamics.common.block.VoxelShapes;
 import electrodynamics.common.block.subtype.SubtypeMachine;
 import electrodynamics.common.inventory.container.tile.ContainerPotentiometer;
 import electrodynamics.prefab.properties.Property;
 import electrodynamics.prefab.properties.PropertyType;
 import electrodynamics.prefab.tile.GenericTile;
 import electrodynamics.prefab.tile.components.type.ComponentContainerProvider;
+import electrodynamics.prefab.tile.components.type.ComponentDirection;
 import electrodynamics.prefab.tile.components.type.ComponentElectrodynamic;
 import electrodynamics.prefab.utilities.object.TransferPack;
 import electrodynamics.registers.ElectrodynamicsBlockTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class TilePotentiometer extends GenericTile {
 
@@ -25,6 +32,7 @@ public class TilePotentiometer extends GenericTile {
 
 	public TilePotentiometer(BlockPos pos, BlockState state) {
 		super(ElectrodynamicsBlockTypes.TILE_POTENTIOMETER.get(), pos, state);
+		addComponent(new ComponentDirection(this));
 		addComponent(new ComponentContainerProvider(SubtypeMachine.potentiometer, this).createMenu((id, player) -> new ContainerPotentiometer(id, player, getCoordsArray())));
 		addComponent(new ComponentElectrodynamic(this).receivePower(this::receivePower).getConnectedLoad(this::getConnectedLoad).input(Direction.NORTH).input(Direction.EAST).input(Direction.SOUTH).input(Direction.WEST).input(Direction.DOWN).voltage(-1.0D));
 	}
@@ -36,12 +44,82 @@ public class TilePotentiometer extends GenericTile {
 		double accepted = Math.min(pack.getJoules(), powerConsumption.get());
 		return TransferPack.joulesVoltage(accepted, pack.getVoltage());
 	}
-	
+
 	private TransferPack getConnectedLoad(LoadProfile loadProfile, Direction dir) {
-		if(dir == Direction.UP || dir == Direction.DOWN) {
+		if (dir == Direction.UP || dir == Direction.DOWN) {
 			return TransferPack.EMPTY;
 		}
 		return TransferPack.joulesVoltage(powerConsumption.get(), -1);
+	}
+
+	static {
+
+		VoxelShape shape = Stream.of(
+				//
+				Stream.of(
+						//
+						Block.box(0, 0, 0, 16, 1, 16),
+						//
+						Block.box(0, 1, 0, 1, 11, 1),
+						//
+						Block.box(0, 1, 15, 1, 11, 16),
+						//
+						Block.box(15, 1, 15, 16, 11, 16),
+						//
+						Block.box(15, 1, 0, 16, 11, 1),
+						//
+						Block.box(1, 10, 0, 4, 11, 1),
+						//
+						Block.box(12, 10, 0, 15, 11, 1),
+						//
+						Block.box(1, 10, 15, 4, 11, 16),
+						//
+						Block.box(12, 10, 15, 15, 11, 16),
+						//
+						Block.box(0, 10, 1, 1, 11, 4),
+						//
+						Block.box(0, 10, 12, 1, 11, 15),
+						//
+						Block.box(15, 10, 1, 16, 11, 4),
+						//
+						Block.box(15, 10, 12, 16, 11, 15)
+				//
+				).reduce((v1, v2) -> Shapes.or(v1, v2)).get(),
+				//
+				Stream.of(
+						//
+						Block.box(2, 1, 2, 14, 12, 14),
+						//
+						Block.box(4, 12, 4, 12, 13, 12),
+						//
+						Block.box(6.5, 13, 6.5, 9.5, 17, 9.5)
+				//
+				).reduce((v1, v2) -> Shapes.or(v1, v2)).get(),
+				//
+				Stream.of(
+						//
+						Block.box(4, 4, 0, 12, 12, 1),
+						//
+						Block.box(5, 5, 1, 11, 11, 2),
+						//
+						Block.box(5, 5, 14, 11, 11, 15),
+						//
+						Block.box(4, 4, 15, 12, 12, 16),
+						//
+						Block.box(0, 4, 4, 1, 12, 12),
+						//
+						Block.box(15, 4, 4, 16, 12, 12),
+						//
+						Block.box(14, 5, 5, 15, 11, 11),
+						//
+						Block.box(1, 5, 5, 2, 11, 11)
+				//
+				).reduce((v1, v2) -> Shapes.or(v1, v2)).get()
+		//
+		).reduce((v1, v2) -> Shapes.or(v1, v2)).get();
+		
+		VoxelShapes.registerShape(SubtypeMachine.potentiometer, shape, Direction.NORTH);
+
 	}
 
 }
