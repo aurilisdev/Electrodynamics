64e2b65c2725fa5f3fb9bead360c39035b3561ad
skip999 <skiprocks999@gmail.com>
2025-06-29 08:37:17 -0500

    fix ore spawn levels

--- src/main/java/electrodynamics/common/settings/OreConfig.java
+++ src/main/java/electrodynamics/common/settings/OreConfig.java
@@ -10,14 +10,10 @@ public class OreConfig {
 	@BooleanValue(def = false)
 	public static boolean DISABLE_ALL_ORES = false;
 
-	// Stone Ores
-	@BooleanValue(def = false)
-	public static boolean DISABLE_STONE_ORES = false;
-
 	@BooleanValue(def = true)
 	public static boolean SPAWN_ALUMINUM_ORE = true;
 	@BooleanValue(def = true)
-	public static boolean SPAWN_COPPER_ORE = false;
+	public static boolean SPAWN_COPPER_ORE = true;
 	@BooleanValue(def = true)
 	public static boolean SPAWN_CHROMIUM_ORE = true;
 	@BooleanValue(def = true)
@@ -50,46 +46,7 @@ public class OreConfig {
 	public static boolean SPAWN_URANIUM_ORE = true;
 	@BooleanValue(def = true)
 	public static boolean SPAWN_VANADIUM_ORE = true;
-
-	// Deepslate Ores
-	@BooleanValue(def = false)
-	public static boolean DISABLE_DEEPSLATE_ORES = false;
-
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_ALUMINUM_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_CHROMIUM_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_FLUORITE_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_LEAD_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_LITHIUM_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_MOLYBDENUM_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_MONAZITE_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_NITER_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_SALT_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_SILVER_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_SULFUR_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_SYLVITE_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_TIN_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_TITANIUM_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_THORIUM_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_URANIUM_ORE = true;
-	@BooleanValue(def = true)
-	public static boolean SPAWN_DEEP_VANADIUM_ORE = true;
-
+	
 	// Multipliers
 	@DoubleValue(def = 1)
 	public static double ORE_GENERATION_MULTIPLIER = 1;
