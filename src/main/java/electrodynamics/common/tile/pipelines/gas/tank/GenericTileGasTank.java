0d5c83648f274163b6b2ddf7d45fd28339061256
aurilisdev <aurilisdev@gmail.com>
2025-10-30 15:01:00 +0100

    Fix gas container insulation (#417)
    Fixes issue #417:
    
    https://github.com/aurilisdev/Electrodynamics/issues/417

--- src/main/java/electrodynamics/common/tile/pipelines/gas/tank/GenericTileGasTank.java
+++ src/main/java/electrodynamics/common/tile/pipelines/gas/tank/GenericTileGasTank.java
@@ -27,8 +27,9 @@ public class GenericTileGasTank extends GenericGasTile {
 	public static final double INSULATION_EFFECTIVENESS = 1.05;
 
 	public static final double HEAT_LOSS = 0.0025; // .05 / 20
-
+	
 	public final SingleProperty<Double> insulationBonus = property(new SingleProperty<>(PropertyTypes.DOUBLE, "insulationbonus", 1.0));
+	
 
 	public GenericTileGasTank(BlockEntityType<?> type, BlockPos pos, BlockState state, SubtypeMachine machine, int capacity, int maxPressure, int maxTemperature) {
 		super(type, pos, state);
@@ -38,6 +39,8 @@ public class GenericTileGasTank extends GenericGasTile {
 		addComponent(new ComponentInventory(this, ComponentInventory.InventoryBuilder.newInv().inputs(6).gasInputs(1).gasOutputs(1)).valid(machineValidator()));
 		addComponent(new ComponentContainerProvider(machine.tag(), this).createMenu((id, player) -> new ContainerGasTankGeneric(id, player, getComponent(IComponentType.Inventory), getCoordsArray())));
 	}
+	
+	private double heatRemainder = 0.0;
 
 	public void tickServer(ComponentTickable tick) {
 		ComponentGasHandlerSimple handler = getComponent(IComponentType.GasHandler);
@@ -47,15 +50,25 @@ public class GenericTileGasTank extends GenericGasTile {
 
 		GasStack gasIn = handler.getGas();
 
-		if (!gasIn.isEmpty() && gasIn.getTemperature() != Gas.ROOM_TEMPERATURE) {
+		if (!gasIn.isEmpty()) {
+		    int temp = gasIn.getTemperature();
+		    int diff = Gas.ROOM_TEMPERATURE - temp;   // positive means we need to heat up
+		    if (diff != 0) {
+			int sign =  (int) Math.signum(diff);
 
-			int deltaT = (int) Math.signum(Gas.ROOM_TEMPERATURE - gasIn.getTemperature());
-
-			int temperatureDecrease = (int) (Math.max(1, HEAT_LOSS / Math.max(1.0, insulationBonus.getValue())) * deltaT);
-
-			handler.heat(0, temperatureDecrease, GasAction.EXECUTE);
-
+		        double step = HEAT_LOSS / insulationBonus.getValue(); // HEAT_LOSS = 0.025
+		        // accumulate toward an integer change, clamp so we never overshoot room temperature
+		        heatRemainder += step;
+		        int whole = (int) Math.floor(heatRemainder);
+		        if (whole > 0) {
+		            int allowed = Math.min(Math.abs(diff), whole);
+		            heatRemainder -= allowed;
+		            int delta = allowed * sign;
+		            handler.heat(0, delta, GasAction.EXECUTE);
+		        }
+		    }
 		}
+
 
 		if (level.getBlockEntity(getBlockPos().below()) instanceof GenericTileGasTank tankBelow) {
 			ComponentGasHandlerSimple belowHandler = tankBelow.getComponent(IComponentType.GasHandler);
