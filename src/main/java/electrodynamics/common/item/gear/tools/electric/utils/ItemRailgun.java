eb890f522d144ea1cd6a097f637e4396149ecc81
aurilisdev <aurilisdev@gmail.com>
2025-12-30 19:54:20 +0100

    Fix Railgun item crash related to temperature colour

--- src/main/java/electrodynamics/common/item/gear/tools/electric/utils/ItemRailgun.java
+++ src/main/java/electrodynamics/common/item/gear/tools/electric/utils/ItemRailgun.java
@@ -35,116 +35,126 @@ import voltaic.registers.VoltaicDataComponentTypes;
 
 public abstract class ItemRailgun extends ItemElectric implements IItemTemperate {
 
-	private static final List<ItemRailgun> ITEMS = new ArrayList<>();
+    private static final List<ItemRailgun> ITEMS = new ArrayList<>();
 
-	public static final Color[]  HEAT_COLORS = {new Color(183, 183, 183, 255), new Color(102, 0, 0, 255), new Color(152, 1, 0, 255), new Color(204, 0 ,1, 255), new Color(253, 51, 1, 255), new Color(255, 102, 51, 255), new Color(254, 154, 100, 255), new Color(255, 203, 102, 255), new Color(254, 204, 50, 255), new Color(255, 255, 101, 255), new Color(255, 255, 153, 255)};
+    public static final Color[] HEAT_COLORS = { new Color(183, 183, 183, 255), new Color(102, 0, 0, 255),
+	    new Color(152, 1, 0, 255), new Color(204, 0, 1, 255), new Color(253, 51, 1, 255),
+	    new Color(255, 102, 51, 255), new Color(254, 154, 100, 255), new Color(255, 203, 102, 255),
+	    new Color(254, 204, 50, 255), new Color(255, 255, 101, 255), new Color(255, 255, 153, 255) };
 
-	public static final int CAPACITY = 5000;
+    public static final int CAPACITY = 5000;
 
-	private final TemperateItemProperties temperateProperties = new TemperateItemProperties();
-	private double overheatTemperature = 0;
-	private double tempThreshold = 0;
-	private double tempPerTick = 0;
+    private final TemperateItemProperties temperateProperties = new TemperateItemProperties();
+    private double overheatTemperature = 0;
+    private double tempThreshold = 0;
+    private double tempPerTick = 0;
 
-	public ItemRailgun(ElectricItemProperties properties, Holder<CreativeModeTab> creativeTab, double overheatTemperature, double tempThreshold, double tempPerTick, Function<Item, Item> getBatteryItem) {
-		super(properties, creativeTab, getBatteryItem);
-		this.overheatTemperature = overheatTemperature;
-		this.tempThreshold = tempThreshold;
-		this.tempPerTick = tempPerTick;
-		ITEMS.add(this);
+    public ItemRailgun(ElectricItemProperties properties, Holder<CreativeModeTab> creativeTab,
+	    double overheatTemperature, double tempThreshold, double tempPerTick, Function<Item, Item> getBatteryItem) {
+	super(properties, creativeTab, getBatteryItem);
+	this.overheatTemperature = overheatTemperature;
+	this.tempThreshold = tempThreshold;
+	this.tempPerTick = tempPerTick;
+	ITEMS.add(this);
+    }
+
+    @Override
+    public void setDamage(ItemStack stack, int damage) {
+
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltip, TooltipFlag flagIn) {
+	super.appendHoverText(stack, context, tooltip, flagIn);
+	tooltip.add(ElectroTextUtils.tooltip("railguntemp", ChatFormatter
+		.getChatDisplayShort(IItemTemperate.getTemperature(stack), DisplayUnits.TEMPERATURE_CELCIUS))
+		.withStyle(ChatFormatting.YELLOW));
+	tooltip.add(ElectroTextUtils
+		.tooltip("railgunmaxtemp",
+			ChatFormatter.getChatDisplayShort(overheatTemperature, DisplayUnits.TEMPERATURE_CELCIUS))
+		.withStyle(ChatFormatting.YELLOW));
+	if (IItemTemperate.getTemperature(stack) >= getOverheatTemp()) {
+	    tooltip.add(ElectroTextUtils.tooltip("railgunoverheat").withStyle(ChatFormatting.RED, ChatFormatting.BOLD));
 	}
+	FluidStack fluid = stack.getOrDefault(VoltaicDataComponentTypes.FLUID_STACK.get(),
+		FluidStackComponent.EMPTY).fluid;
+	tooltip.add(VoltaicTextUtils.ratio(ChatFormatter.formatFluidMilibuckets(fluid.getAmount()),
+		ChatFormatter.formatFluidMilibuckets(CAPACITY)).withStyle(ChatFormatting.GRAY));
+    }
 
-	@Override
-	public void setDamage(ItemStack stack, int damage) {
-
+    @Override
+    public void addCreativeModeItems(CreativeModeTab group, List<ItemStack> items) {
+	List<ItemStack> superItems = new ArrayList<>();
+	super.addCreativeModeItems(group, superItems);
+	for (ItemStack stack : superItems) {
+	    if (stack.getItem() instanceof ItemRailgun) {
+		IItemTemperate.setTemperature(stack, 0);
+	    }
 	}
+	items.addAll(superItems);
+    }
 
-	@Override
-	public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltip, TooltipFlag flagIn) {
-		super.appendHoverText(stack, context, tooltip, flagIn);
-		tooltip.add(ElectroTextUtils.tooltip("railguntemp", ChatFormatter.getChatDisplayShort(IItemTemperate.getTemperature(stack), DisplayUnits.TEMPERATURE_CELCIUS)).withStyle(ChatFormatting.YELLOW));
-		tooltip.add(ElectroTextUtils.tooltip("railgunmaxtemp", ChatFormatter.getChatDisplayShort(overheatTemperature, DisplayUnits.TEMPERATURE_CELCIUS)).withStyle(ChatFormatting.YELLOW));
-		if (IItemTemperate.getTemperature(stack) >= getOverheatTemp()) {
-			tooltip.add(ElectroTextUtils.tooltip("railgunoverheat").withStyle(ChatFormatting.RED, ChatFormatting.BOLD));
-		}
-		FluidStack fluid = stack.getOrDefault(VoltaicDataComponentTypes.FLUID_STACK.get(), FluidStackComponent.EMPTY).fluid;
-		tooltip.add(VoltaicTextUtils.ratio(ChatFormatter.formatFluidMilibuckets(fluid.getAmount()), ChatFormatter.formatFluidMilibuckets(CAPACITY)).withStyle(ChatFormatting.GRAY));
-	}
+    @Override
+    public void inventoryTick(ItemStack stack, Level worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+	super.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
+	((ItemRailgun) stack.getItem()).loseHeat(stack, tempPerTick, 0.0, false);
+    }
 
-	@Override
-	public void addCreativeModeItems(CreativeModeTab group, List<ItemStack> items) {
-		List<ItemStack> superItems = new ArrayList<>();
-		super.addCreativeModeItems(group, superItems);
-		for (ItemStack stack : superItems) {
-			if (stack.getItem() instanceof ItemRailgun) {
-				IItemTemperate.setTemperature(stack, 0);
-			}
-		}
-		items.addAll(superItems);
-	}
+    @Override
+    public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
+	return !oldStack.is(newStack.getItem());
+    }
 
-	@Override
-	public void inventoryTick(ItemStack stack, Level worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
-		super.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
-		((ItemRailgun) stack.getItem()).loseHeat(stack, tempPerTick, 0.0, false);
-	}
+    public double getMaxTemp() {
+	return overheatTemperature;
+    }
 
-	@Override
-	public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
-		return !oldStack.is(newStack.getItem());
-	}
+    public double getOverheatTemp() {
+	return overheatTemperature * tempThreshold;
+    }
 
-	public double getMaxTemp() {
-		return overheatTemperature;
-	}
+    @Override
+    public TemperateItemProperties getTemperteProperties() {
+	return temperateProperties;
+    }
 
-	public double getOverheatTemp() {
-		return overheatTemperature * tempThreshold;
-	}
+    public static Predicate<FluidStack> getPredicate() {
+	return fluid -> fluid.getFluid().builtInRegistryHolder().is(VoltaicTags.Fluids.AMMONIA);
+    }
 
-	@Override
-	public TemperateItemProperties getTemperteProperties() {
-		return temperateProperties;
-	}
+    @EventBusSubscriber(value = Dist.CLIENT, modid = Electrodynamics.ID, bus = EventBusSubscriber.Bus.MOD)
+    private static class ColorHandler {
 
-	public static Predicate<FluidStack> getPredicate() {
-		return fluid -> fluid.getFluid().builtInRegistryHolder().is(VoltaicTags.Fluids.AMMONIA);
-	}
-
-	@EventBusSubscriber(value = Dist.CLIENT, modid = Electrodynamics.ID, bus = EventBusSubscriber.Bus.MOD)
-	private static class ColorHandler {
-
-		@SubscribeEvent
-		public static void registerColoredBlocks(RegisterColorHandlersEvent.Item event) {
-			ITEMS.forEach(item -> event.register((stack, index) -> {
-				if (index != 1) {
-					return Color.WHITE.color();
-				}
-
-				double currHeat = IItemTemperate.getTemperature(stack);
-
-				if(currHeat <= 0){
-					return HEAT_COLORS[0].color();
-				}
-
-				double maxHeat = item.getMaxTemp();
-
-				double amtPerTier = maxHeat / HEAT_COLORS.length;
-
-				int threshhold = (int) (currHeat / amtPerTier);
-
-				if(threshhold == HEAT_COLORS.length - 1){
-					return HEAT_COLORS[threshhold].color();
-				}
-
-				double amtNextTier = (currHeat - amtPerTier * threshhold) / amtPerTier;
-
-				return HEAT_COLORS[threshhold].blend(HEAT_COLORS[threshhold + 1], amtNextTier).color();
-
-
-			}, item));
+	@SubscribeEvent
+	public static void registerColoredBlocks(RegisterColorHandlersEvent.Item event) {
+	    ITEMS.forEach(item -> event.register((stack, index) -> {
+		if (index != 1) {
+		    return Color.WHITE.color();
 		}
 
+		double currHeat = IItemTemperate.getTemperature(stack);
+
+		if (currHeat <= 0) {
+		    return HEAT_COLORS[0].color();
+		}
+
+		double maxHeat = item.getMaxTemp();
+
+		double amtPerTier = maxHeat / HEAT_COLORS.length;
+
+		int threshhold = (int) (currHeat / amtPerTier);
+
+		if (threshhold >= HEAT_COLORS.length - 1) {
+		    return HEAT_COLORS[threshhold].color();
+		}
+
+		double amtNextTier = (currHeat - amtPerTier * threshhold) / amtPerTier;
+
+		return HEAT_COLORS[threshhold].blend(HEAT_COLORS[threshhold + 1], amtNextTier).color();
+
+	    }, item));
 	}
+
+    }
 
 }
